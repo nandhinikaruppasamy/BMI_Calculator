51b7a140eebfc9f923a1342992803f3da55e0894
Nandhini <nandhinikaruppasamy1402@gmail.com>
2023-09-25 22:17:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.getElementById('calculate');
+btn.addEventListener('click', function () {
+    let height = document.querySelector('#height').value;
+    let weight = document.querySelector('#weight').value;
+
+    if (height == '' || weight == '') {
+        alert("Please fill out the input fields ! ");
+        return;
+    }
+    height = height / 100;
+    let BMI = (weight / (height * height));
+    BMI = BMI.toFixed(2);
+    document.querySelector('#result').innerHTML = BMI;
+    let status = '';
+    if (BMI < 18.5) {
+        status = "UnderWeight";
+    }
+    if (BMI >= 18.5 && BMI < 25) {
+        status = "Healthy";
+    }
+    if (BMI >= 25 && BMI < 30) {
+        status = "OverWeight";
+    }
+    if (BMI >= 30) {
+        status = "Obese";
+    }
+    document.querySelector('.comment').innerHTML = `Comment : You are <span id ="comment">${status}<span>`;
+
+
+})
